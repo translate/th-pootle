0310054f6dcd506859b1a0501cb4f5964e373fa4
Ryan Northey <ryan@synca.io>
2017-02-12 13:49:42 +0000

    Add FastMigration utils

--- th_pootle/utils.py
+++ th_pootle/utils.py
@@ -6,13 +6,268 @@
 # or later license. See the LICENSE file for a copy of the license and the
 # AUTHORS file for copyright and authorship information.
 
+import codecs
 import contextlib
 import csv
+import cStringIO
+import os
 import time
+import uuid
+from collections import OrderedDict
+
+from django import db
+from django.utils.functional import cached_property
+
+
+class UTF8CSVRecoder:
+    """
+    Iterator that reads an encoded stream and reencodes the input to UTF-8
+    """
+    def __init__(self, f, encoding):
+        self.reader = codecs.getreader(encoding)(f)
+
+    def __iter__(self):
+        return self
+
+    def next(self):
+        return self.reader.next().encode("utf-8")
+
+
+class UnicodeCSVReader:
+    """
+    A CSV reader which will iterate over lines in the CSV file "f",
+    which is encoded in the given encoding.
+    """
+
+    def __init__(self, f, dialect=csv.excel, encoding="utf-8", **kwds):
+        f = UTF8CSVRecoder(f, encoding)
+        self.reader = csv.reader(f, dialect=dialect, **kwds)
+
+    def next(self):
+        row = self.reader.next()
+        return [unicode(s, "utf-8") for s in row]
+
+    def __iter__(self):
+        return self
+
+
+class MySqlCSVWriter:
+
+    def __init__(self, f, dialect=csv.excel, encoding="utf-8", **kwds):
+        # Redirect output to a queue
+        self.queue = cStringIO.StringIO()
+        self.writer = csv.writer(self.queue, dialect=dialect, **kwds)
+        self.stream = f
+        self.encoder = codecs.getincrementalencoder(encoding)()
+
+    def writerow(self, row):
+        self.writer.writerow([s.encode("utf-8") for s in row])
+        # Fetch UTF-8 output from the queue ...
+        data = self.queue.getvalue()
+        data = data.decode("utf-8")
+        # ... and reencode it into the target encoding
+        data = self.encoder.encode(data)
+        data = data.replace("__☠__", b'\N')
+        data = data.replace("\\n", b'\n')
+        data = data.replace('\\"', b'\"')
+        data = data.replace("\\,", ",")
+        data = data.replace("\\\\", "\\")
+        # write to the target stream
+        self.stream.write(data)
+        # empty queue
+        self.queue.truncate(0)
+
+    def writerows(self, rows):
+        for row in rows:
+            self.writerow(row)
 
 
 class FastMigration(object):
-    pass
+
+    def fpath(self, path, name, k):
+        return os.path.join(
+            path, "%s.%s.txt" % (k, name))
+
+    def migrate(self, actions=None, name=None, path="/tmp"):
+        name = name or uuid.uuid4().hex[:10]
+        if not actions or "dump" in actions:
+            for dumped in self.dump_data(path, name):
+                yield "dump", dumped
+        if not actions or "schema" in actions:
+            for schema in self.dump_schema(path, name):
+                yield "schema", schema
+        if not actions or "mangle" in actions:
+            for parsed in self.parse_data(path, name):
+                yield "parse", parsed
+            for mangled in self.mangle_data(path, name):
+                yield mangled["action"], mangled
+        elif "parse" in actions:
+            for parsed in self.parse_data(path, name):
+                yield "parse", parsed
+        if not actions or "create" in actions:
+            for created in self.create_data(path, name):
+                yield "create", created
+        if not actions or "alter" in actions:
+            for altered in self.alter_data(path, name):
+                yield "alter", altered
+        if not actions or "load" in actions:
+            for loaded in self.load_data(path, name):
+                yield "load", loaded
+        if actions and "validate" in actions:
+            for validated in self.validate_data(path, name):
+                yield "validate", validated
+
+    def dump_data(self, path, name):
+        for k, dump in self.dump.items():
+            yield self.outfile.select(
+                dump.pop("table"),
+                self.fpath(path, name, k),
+                dump.pop("columns", None),
+                **dump)
+
+    def dump_schema(self, path, name):
+        for k, dump in self.schema.items():
+            yield self.outfile.select_schema(
+                dump.pop("table"),
+                self.fpath(path, name, "schema__%s" % k),
+                dump.pop("columns", None),
+                **dump)
+
+    def parse_data(self, path, name):
+        self.parsed = {}
+        for k, data in self.data.items():
+            if data.pop("schema", None):
+                k = "schema__%s" % k
+            result = self.csv_loader(
+                self.fpath(path, name, k)).as_dict(
+                    loader=data.pop("loader", None),
+                    **data)
+            result["name"] = k
+            self.parsed[k] = result["results"]
+            del result["results"]
+            yield result
+
+    def mangle_data(self, path, name):
+        for k, mangle in self.mangle.items():
+            target = mangle.pop("target", None)
+            after = mangle.pop("after", None)
+            parse_extra = mangle.pop("parse", [])
+            for parse in parse_extra:
+                result = self.csv_loader(
+                    self.fpath(path, name, parse)).as_dict()
+                result["action"] = "parse"
+                result["name"] = parse
+                self.parsed[parse] = result["results"]
+                del result["results"]
+                yield result
+            if target:
+                target = self.fpath(path, name, target)
+            mangler = self.csv_mangler(
+                self.fpath(path, name, mangle.pop("source")),
+                target,
+                **mangle)
+            mangled = mangler.mangle(mangle["mangler"])
+            mangled["name"] = k
+            mangled["action"] = "mangle"
+            if after:
+                after()
+            if not target:
+                self.parsed["mangled__%s" % k] = mangled
+            yield mangled
+
+    def create_data(self, path, name):
+        for k, create in self.create.items():
+            source = create.pop("source")
+            if self.manager.rowcount(source) is None:
+                result = self.manager.copy_table(
+                    source, k)
+                yield result
+
+    def alter_data(self, path, name):
+        for k, alter in self.alter.items():
+            start = time.time()
+            action = alter[0]
+            if action == "remove":
+                try:
+                    result = self.manager.drop_columns(k, alter[1])
+                except db.OperationalError:
+                    # most likely the column doesnt exist
+                    pass
+            yield dict(
+                table=k,
+                result=result,
+                columns=alter[1],
+                timing=(time.time() - start))
+
+    def load_data(self, path, name):
+        for k, load in self.load.items():
+            result = self.infile.load(
+                load.pop("table"),
+                self.fpath(path, name, k),
+                local=load.pop("local", False),
+                **load)
+            yield result
+
+    def validate_data(self, path, name):
+        start = time.time()
+        for k, validate in self.validate.items():
+            check_count = validate.get("check_count", False)
+            step = validate.get("step", 10000)
+            self.cursor.execute("select count(*) from %s" % k)
+            total = self.cursor.fetchone()[0]
+            if check_count:
+                assert validate["model"].objects.count() == total
+            i = 0
+            columns = validate.get("columns", ["*"])
+            while True:
+                self.cursor.execute(
+                    "select %s from %s limit %s offset %s"
+                    % (", ".join(columns), k, step, i + step))
+                result = self.cursor.fetchall()
+                existing = tuple(
+                    validate["model"].objects.filter(
+                        id__in=[
+                            res[0]
+                            for res
+                            in result]).values_list(*columns))
+                existing = {e[0]: e[1:] for e in existing}
+                result = {r[0]: r[1:] for r in result}
+                assert existing == result
+                i += step
+                yield dict(
+                    action="validate",
+                    table=k,
+                    columns=", ".join(columns),
+                    model=str(validate["model"]._meta),
+                    count=min(i, total),
+                    total=total,
+                    timing=(time.time() - start))
+                if i > total:
+                    break
+
+    @cached_property
+    def cursor(self):
+        return db.connection.cursor()
+
+    @cached_property
+    def infile(self):
+        return MySqlInfile(self, self.cursor)
+
+    @cached_property
+    def outfile(self):
+        return MySqlOutfile(self, self.cursor)
+
+    @cached_property
+    def manager(self):
+        return MySqlManager(self, self.cursor)
+
+    @property
+    def csv_loader(self):
+        return CSVLoader
+
+    @property
+    def csv_mangler(self):
+        return CSVMangler
 
 
 class CSVLoader(object):
@@ -21,13 +276,16 @@ class CSVLoader(object):
         self.source = source
         self.kwargs = kwargs
 
-    def as_dict(self, loader=None):
+    def as_dict(self, loader=None, ordered=False):
         start = time.time()
-        results = {}
+        results = (
+            OrderedDict()
+            if ordered
+            else {})
         total = 0
         with open(self.source, "rb") as f:
-            unit_data = csv.reader(f)
-            for row in unit_data:
+            parsed_data = csv.reader(f)
+            for row in parsed_data:
                 total += 1
                 if loader:
                     result = loader(*row)
@@ -38,6 +296,8 @@ class CSVLoader(object):
                     results[row[0]] = row[1]
                 elif len(row) > 2:
                     results[row[0]] = row[1:]
+                elif row:
+                    results[row[0]] = None
         return dict(
             source=self.source,
             total=total,
@@ -64,25 +324,68 @@ class CSVMangler(object):
         return self.kwargs.get("writer_kwargs", {})
 
     def mangle(self, mangler):
+        if self.target:
+            return self.mangle_to_file(mangler)
+        return self.mangle_to_mem(mangler)
+
+    def mangle_to_mem(self, mangler):
+        start = time.time()
+        count = 0
+        total = 0
+        results = []
+        for result in self.iter_source(mangler):
+            total += 1
+            if result is not None:
+                count += 1
+                results.append(result)
+        return dict(
+            source=self.source,
+            target=":memory:",
+            count=count,
+            results=results,
+            total=total,
+            timing=(time.time() - start))
+
+    def iter_source(self, mangler):
+        with open(self.source, "rb") as rf:
+            reader = self.reader(rf, **self.reader_kwargs)
+            for row in reader:
+                yield mangler(*row)
+
+    def mangle_to_file(self, mangler):
         start = time.time()
         count = 0
         total = 0
         with open(self.target, "wb") as wf:
             writer = self.writer(wf, **self.writer_kwargs)
-            with open(self.source, "rb") as rf:
-                reader = self.reader(rf, **self.reader_kwargs)
-                for row in reader:
-                    total += 1
-                    result = mangler(*row)
-                    if result is not None:
-                        count += 1
-                        writer.writerow(result)
+            for result in self.iter_source(mangler):
+                total += 1
+                if result is not None:
+                    count += 1
+                    writer.writerow(result)
         return dict(
             source=self.source,
             target=self.target,
             count=count,
             total=total,
             timing=(time.time() - start))
+
+
+@contextlib.contextmanager
+def fast_load(cursor):
+    try:
+        cursor.execute("SET GLOBAL sync_binlog=0;")
+        cursor.execute("SET GLOBAL innodb_flush_log_at_trx_commit=2;")
+        cursor.execute("SET UNIQUE_CHECKS=0;")
+        cursor.execute("SET FOREIGN_KEY_CHECKS=0;")
+        cursor.execute("SET SESSION tx_isolation='READ-UNCOMMITTED';")
+        yield
+    finally:
+        cursor.execute("SET SESSION tx_isolation='REPEATABLE-READ';")
+        cursor.execute("SET FOREIGN_KEY_CHECKS=1;")
+        cursor.execute("SET UNIQUE_CHECKS=1;")
+        cursor.execute("SET GLOBAL innodb_flush_log_at_trx_commit=1;")
+        cursor.execute("SET GLOBAL sync_binlog=1;")
 
 
 @contextlib.contextmanager
@@ -94,6 +397,15 @@ def no_fk_checks(cursor):
         cursor.execute("SET FOREIGN_KEY_CHECKS = 1;")
 
 
+@contextlib.contextmanager
+def table_lock(cursor, table):
+    try:
+        cursor.execute("LOCK TABLE %s WRITE;" % table)
+        yield
+    finally:
+        cursor.execute("UNLOCK TABLES;")
+
+
 class MySqlBase(object):
 
     def execute(self, sql):
@@ -103,7 +415,8 @@ class MySqlBase(object):
 
 class MySqlManager(MySqlBase):
 
-    def __init__(self, cursor, **kwargs):
+    def __init__(self, context, cursor, **kwargs):
+        self.context = context
         self.cursor = cursor
 
     def truncate_table(self, table):
@@ -112,11 +425,26 @@ class MySqlManager(MySqlBase):
                 "truncate table %s; "
                 % table)
 
+    def drop_columns(self, table, columns):
+        column_sql = ", ".join(
+            "DROP COLUMN %s" % c for c in columns)
+        self.cursor.execute(
+            "alter table %s %s"
+            % (table, column_sql))
+
     def copy_table(self, source, target):
         self.cursor.execute(
-            "create table %s like "
+            "CREATE TABLE %s LIKE "
             "%s;"
             % (target, source))
+
+    def rowcount(self, table):
+        try:
+            self.cursor.execute(
+                "select count(*) from %s" % table)
+        except db.ProgrammingError:
+            return None
+        return self.cursor.fetchone()[0]
 
 
 class MySqlFile(MySqlBase):
@@ -125,7 +453,8 @@ class MySqlFile(MySqlBase):
     line_terminator = "\\n"
     optionally_enclose = '"'
 
-    def __init__(self, cursor, **kwargs):
+    def __init__(self, context, cursor, **kwargs):
+        self.context = context
         self.cursor = cursor
         self.field_terminator = kwargs.get(
             "field_terminator", self.field_terminator)
@@ -150,7 +479,7 @@ class MySqlOutfile(MySqlFile):
     def get_columns(self, columns):
         if columns is None:
             return "*"
-        return ", ".join(columns)
+        return ", ".join("`%s`" % c for c in columns)
 
     @property
     def dbname(self):
@@ -173,7 +502,14 @@ class MySqlOutfile(MySqlFile):
             return ""
         clauses = []
         for k, v in where.items():
-            clauses.append("%s = '%s'" % (k, v))
+            oper = " = "
+            if k.endswith("__gt"):
+                k = k[:-4]
+                oper = " > "
+            if k.endswith("__ne"):
+                k = k[:-4]
+                oper = " != "
+            clauses.append("%s%s'%s'" % (k, oper, v))
         return "WHERE %s " % " AND ".join(clauses)
 
     def generate_sql(self, table, outfile, columns, **kwargs):
@@ -204,10 +540,11 @@ class MySqlOutfile(MySqlFile):
                 **kwargs))
         return dict(
             filepath=outfile,
+            table=table,
             rows=rows,
             timing=(time.time() - start))
 
-    def select_schema(self, table, outfile, **kwargs):
+    def select_schema(self, table, outfile, columns, **kwargs):
         start = time.time()
         table_name = table
         table = "information_schema.COLUMNS"
@@ -215,7 +552,7 @@ class MySqlOutfile(MySqlFile):
             self.generate_sql(
                 table,
                 outfile,
-                self.get_columns(["column_name"]),
+                self.get_columns(columns),
                 where=dict(
                     table_name=table_name,
                     table_schema=self.dbname),
@@ -223,6 +560,7 @@ class MySqlOutfile(MySqlFile):
         return dict(
             filepath=outfile,
             rows=rows,
+            table=table,
             timing=(time.time() - start))
 
 
@@ -238,8 +576,9 @@ class MySqlInfile(MySqlFile):
         return (
             "LOAD DATA %sINFILE '%s' into table %s "
             "FIELDS TERMINATED BY '%s' "
+            "ESCAPED BY '\\\\' "
             "%s"
-            "LINES TERMINATED BY '%s' "
+            "LINES TERMINATED BY '%s'; "
             % (self.get_local(local),
                infile,
                table,
@@ -249,12 +588,31 @@ class MySqlInfile(MySqlFile):
 
     def load(self, table, infile, **kwargs):
         start = time.time()
-        rows = self.execute(
-            self.generate_sql(
-                table,
-                infile,
-                kwargs.get("local", False)))
+        count = self.context.manager.rowcount(table)
+        if count and not kwargs.get("force"):
+            raise db.IntegrityError(
+                "Target table '%s' contains data and "
+                "force not specificed" % table)
+        elif count:
+            self.context.manager.truncate_table(table)
+        elif count is None:
+            if kwargs.get("create", False):
+                self.context.manager.copy_table(
+                    kwargs.pop("source_table"), table)
+            else:
+                raise db.OperationalError(
+                    "Target table '%s' does not exist "
+                    "and create not specificed" % table)
+
+        with table_lock(self, table):
+            with fast_load(self):
+                rows = self.execute(
+                    self.generate_sql(
+                        table,
+                        infile,
+                        kwargs.get("local", False)))
         return dict(
             filepath=infile,
             rows=rows,
+            table=table,
             timing=(time.time() - start))
